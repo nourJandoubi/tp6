42d1b87e9fcbfa4d529b376574718640c1daeb72
nour jandoubi <jandoubi.nourelhouda@gmail.com>
2021-10-25 14:48:01 +0100

    tp6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './application/accueil/accueil.component';
+import { ContactComponent } from './application/contact/contact.component';
+import { ListproduitsComponent } from './application/listproduits/listproduits.component';
+import { Error404Component } from './application/error404/error404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'accueil', component:AccueilComponent},
+  {path:'produits', component:ListproduitsComponent},
+  {path:'contact', component:ContactComponent},
+  {path:'', redirectTo:'accueil', pathMatch:'full'},
+  {path:'**', component:Error404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
